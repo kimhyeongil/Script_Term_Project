11d4d765656d0c3fcb98bc35d0b3d5e9596cfb72
KHI(home) <alfkcjstk853@naver.com>
2024-05-23 22:36:07 +0900

    메인 생성

---
+++ find_charger.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+class MainGUI:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Find Charger')
+        self.window.geometry('800x600')
+        self.window.mainloop()
+
+MainGUI()
