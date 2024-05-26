1fb179ddaa2a139f10139b162993379e85654e3a
KHI(home) <alfkcjstk853@naver.com>
2024-05-26 16:28:03 +0900

    선택한 충전소의 급속, 완속 충전기 개수를 그래프로 표시

--- find_charger.py
+++ find_charger.py
@@ -9,7 +9,7 @@ class MainGUI:
     def __init__(self):
         self.window = Tk()
         self.window.title('Find Charger')
-        self.window.geometry('1000x600')
+        self.window.geometry('1024x624')
 
         self.url = "https://bigdata.kepco.co.kr/openapi/v1/EVcharge.do"
         self.key = "GqAUvg9r8nJl20eWk533DCrJwwcbm81kst6Z0fEW"
@@ -48,10 +48,11 @@ class MainGUI:
 
     def showChargeList(self):
         city = self.cityCombobox.get()
-        for i in range(4):
-            if i + self.page * 4 < len(self.chargeInfos[city]):
+        for i in range(len(self.chargeLabels)):
+            index = i + self.page * len(self.chargeLabels)
+            if index < len(self.chargeInfos[city]):
                 self.chargeLabels[i][
-                    'text'] = f"주소: {self.chargeInfos[city][i + self.page * 4]['stnAddr']}\n장소: {self.chargeInfos[city][i + self.page * 4]['stnPlace']}"
+                    'text'] = f"주소: {self.chargeInfos[city][index]['stnAddr']}\n장소: {self.chargeInfos[city][index]['stnPlace']}"
             else:
                 self.chargeLabels[i]['text'] = ''
 
@@ -61,15 +62,36 @@ class MainGUI:
 
     def OnInfoListLabelClick(self, event, index):
         city = self.cityCombobox.get()
+        index = index + self.page * len(self.chargeLabels)
         if city in self.cities:
-            if index + self.page * 4 < len(self.chargeInfos[city]):
+            if index < len(self.chargeInfos[city]):
                 self.infoCanvas.delete('all')
-                self.infoCanvas.create_text(100, 30,
-                                            text=str(self.chargeInfos[city][index + self.page * 4]['rapidCnt']))
+
+                self.infoCanvas.create_text(10, 20, text=self.chargeInfos[city][index]['stnAddr'],
+                                            anchor="w", font=('consolas', 12, 'bold'))
+                offset = int(self.infoCanvas['height']) - 50
+                height = 35
+
+                rapidCnt = self.chargeInfos[city][index]['rapidCnt']
+                self.infoCanvas.create_rectangle(100, offset, 200, offset - height * rapidCnt, fill='red')
+                self.infoCanvas.create_text(106, offset + 20, text='급속 충전기',
+                                            anchor="w", font=('consolas', 12, 'bold'))
+                self.infoCanvas.create_text(150, offset - height * rapidCnt - 20, text=str(rapidCnt),
+                                            font=('consolas', 12, 'bold'))
+
+                slowCnt = self.chargeInfos[city][index]['slowCnt']
+                self.infoCanvas.create_rectangle(300, offset, 400,
+                                                 offset - height * slowCnt,
+                                                 fill='red')
+                self.infoCanvas.create_text(306, offset + 20, text='완속 충전기',
+                                            anchor="w", font=('consolas', 12, 'bold'))
+                self.infoCanvas.create_text(350, offset - height * slowCnt - 20, text=str(slowCnt),
+                                            font=('consolas', 12, 'bold'))
 
     def nextPage(self):
         if self.cityCombobox.get() in self.cities:
-            self.page = min(self.page + 1, (len(self.chargeInfos[self.cityCombobox.get()]) - 1) // 4)
+            self.page = min(self.page + 1,
+                            (len(self.chargeInfos[self.cityCombobox.get()]) - 1) // len(self.chargeLabels))
             self.showChargeList()
 
     def prevPage(self):
@@ -94,7 +116,7 @@ class MainGUI:
         self.menuFrame.place(x=25, y=50)
 
     def initSearch(self):
-        self.searchFrame = Frame(self.window, width=1000, height=600)
+        self.searchFrame = Frame(self.window, width=1024, height=624)
 
         self.cityCombobox = Combobox(self.searchFrame)
         self.cityCombobox.set('시/군')
@@ -102,36 +124,36 @@ class MainGUI:
         self.cityCombobox.bind("<<ComboboxSelected>>", self.OnComboboxSelect)
         self.cityCombobox.place(x=0, y=10)
 
-        self.chargeLabels = [Label(self.searchFrame, width=34, height=7, wraplength=270,
+        self.chargeLabels = [Label(self.searchFrame, width=28, height=4, wraplength=260,
                                    font=('arial', 12, 'bold'), anchor="nw", justify="left",
                                    bg='#f9f6f2' if i & 1 else '#D3D3D3')
-                             for i in range(4)]
-        for i in range(4):
+                             for i in range(6)]
+        for i in range(len(self.chargeLabels)):
             self.chargeLabels[i].bind("<Button-1>", lambda event, index=i: self.OnInfoListLabelClick(event, index))
-            self.chargeLabels[i].place(x=0, y=50 + (i * 16 * 7))
+            self.chargeLabels[i].place(x=0, y=50 + (i * 20 * 4))
 
         self.prevImg = PhotoImage(file='왼쪽이동.png')
         self.nextImg = PhotoImage(file='오른쪽이동.png')
         self.prevButton = Button(self.searchFrame, bg='white', image=self.prevImg, command=self.prevPage)
         self.nextButton = Button(self.searchFrame, bg='white', image=self.nextImg, command=self.nextPage)
 
-        self.prevButton.place(x=50, y=50 + (4 * 16 * 7))
-        self.nextButton.place(x=124, y=50 + (4 * 16 * 7))
+        self.prevButton.place(x=72, y=50 + (6 * 20 * 4))
+        self.nextButton.place(x=146, y=50 + (6 * 20 * 4))
 
-        self.infoCanvas = Canvas(self.searchFrame, width=475, height=475, bg='white')
-        self.infoCanvas.place(x=275, y=10)
+        self.infoCanvas = Canvas(self.searchFrame, width=500, height=500, bg='white')
+        self.infoCanvas.place(x=300, y=5)
 
         self.mailImg = PhotoImage(file='이메일.png')
         self.mailButton = Button(self.searchFrame, bg='white', image=self.mailImg)
-        self.mailButton.place(x=475, y=500)
+        self.mailButton.place(x=490, y=515)
 
         self.mapImg = PhotoImage(file='지도.png')
         self.mapButton = Button(self.searchFrame, bg='white', image=self.mapImg)
-        self.mapButton.place(x=325, y=500)
+        self.mapButton.place(x=340, y=515)
 
         self.telegramImg = PhotoImage(file='텔레그램.png')
         self.telegramButton = Button(self.searchFrame, bg='white', image=self.telegramImg)
-        self.telegramButton.place(x=625, y=500)
+        self.telegramButton.place(x=640, y=515)
 
         self.searchFrame.place(x=215, y=10)
 
