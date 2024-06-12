29db3e43fb19098ef358123ab0f4ea09d5bfb013
KHI(home) <alfkcjstk853@naver.com>
2024-06-12 20:05:22 +0900

    텔레그램 봇에 즐겨찾기 관련 명령 추가

--- telegram.py
+++ telegram.py
@@ -29,5 +29,24 @@ def handle(msg):
                 send(response)
         else:
             send('경기도엔 그런 지역이 없습니다.')
+    elif text.startswith('즐찾추가') and len(args) > 2:
+        place = ''
+        for i in range(2, len(args)):
+            place += args[i]
+        if args[1] in Data.chargeInfos:
+            for info in Data.chargeInfos[args[1]]:
+                if info['stnPlace'] == place:
+                    Data.bookmarkCities.append(info)
+                    send('즐겨찾기에 추가했습니다.')
+                    return
+            send(f'{args[1]}에는 {place}가 없습니다.')
+
+    elif text.startswith('즐찾보기'):
+        for info in Data.bookmarkCities:
+            response = '주소: ' + info['stnAddr'] + '\n'
+            response += '장소: ' + info['stnPlace'] + '\n'
+            response += '급속 충전기: ' + str(info['rapidCnt']) + '\n'
+            response += '완속 충전기: ' + str(info['slowCnt']) + '\n'
+            send(response)
     else:
         send('모르는 명령어입니다.\n지역 [시/군], 즐찾추가 [시/군] [장소], 즐찾보기 중 한 명령을 입력하세요.')
